2feb99deb13dfda879fc46988a4a68c65a9f5380
John Doe <nguyenminhphuc21150202@gmail.com>
2017-08-26 22:52:22 +0700

    ád

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+    <title>Hoc PHP</title>
     </head>
     <body>
-        
-        helo
-        
+        <?php
+            echo "<p>Xin chao PHP! </p>";
+        ?>
     </body>
 </html>
